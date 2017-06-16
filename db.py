410ae29f9effc98a5a19cb91902653bc31e79cd4
kwei <ywk991112@gmail.com>
2017-06-16 19:35:58 +0800

    Update db.py

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ db = {"kwei": {"is_enemy": True, "weakness": "stomach"},
       "chou": {"is_enemy": False, "weakness": "head"}}
 
 
-KEY = '10d75bef5fbb4470b000a890ab95815c'
+KEY = ''
 Url = 'westcentralus.api.cognitive.microsoft.com'
 personGroupId = 0
